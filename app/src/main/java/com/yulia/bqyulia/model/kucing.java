37a3ee15073652b9da49f4057cdc2fe5647ee811
windaazhari <winda.azhari2019@studenhamzanwadi.ac.id>
2020-11-26 18:27:42 +0800

    ini commit pertama saya

---
+++ app/src/main/java/com/yulia/bqyulia/model/kucing.java
@@ -0,0 +1,7 @@
+package com.yulia.bqyulia.model;
+
+public class kucing extends hewan{
+    public kucing(String ras, String asal, String deskripsi, int drawableRes) {
+        super("Kucing",ras,asal,deskripsi,drawableRes);
+    }
+}
